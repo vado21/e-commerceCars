4d70a12bdacf3d9425125643d1c773f2666306b0
vado21 <vado21>
2021-11-11 20:29:33 -0600

    Cambios

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app>
+  <div >
+    <v-app-bar
+      color="deep-purple accent-4"
+      dark
+      dense
+    >
+
+      <v-toolbar-title>Tienda Virtual de Carros</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+        
+      
+    </v-app-bar>
+    <v-navigation-drawer v-show="false" app>
       <navigation-bar/>
     </v-navigation-drawer >
     <v-content>
       <router-view/>
     </v-content>
-  </v-app>
+  </div>
 </template>
 
 <script>
